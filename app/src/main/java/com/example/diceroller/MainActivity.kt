4f054d23e1b50193f3bbf980a4bc848e3e21395a
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-02-25 11:09:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val mGenerator = Random()
+
+    val diceIds = arrayOf(0, R.drawable.dice1, R.drawable.dice2, R.drawable.dice3,
+                                R.drawable.dice4, R.drawable.dice5, R.drawable.dice6 )
+
+    val mHistory = mutableListOf<Pair<Int, Int>>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickRoll(v: View){
+        val d1 = mGenerator.nextInt(6) + 1
+        val d2 = mGenerator.nextInt(6) + 1
+
+        imgDice1.setImageResource(diceIds[d1])
+        imgDice2.setImageResource(diceIds[d2])
+
+        if (mHistory.size >= 5)
+            mHistory.removeAt(0)
+        mHistory.add(Pair(d1, d2))
+        updateHistory()
+    }
+
+    private fun updateHistory(){
+        var s = ""
+        mHistory.forEach{ p -> val e1 = p.first; val e2 = p.second
+                                    s += "$e1 - $e2\n"}
+        tvHistory.text = s
+    }
+
+
+
+    fun onClickClear(v: View){
+        mHistory.clear()
+        updateHistory()
+    }
+}
