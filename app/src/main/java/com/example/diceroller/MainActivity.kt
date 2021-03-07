4211ed50da8c05d79c2e94cd252b5b7ca55491be
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-03-07 16:09:17 +0100

    Spinner Added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -18,14 +20,27 @@ class MainActivity : AppCompatActivity() {
                                 R.drawable.dice4, R.drawable.dice5, R.drawable.dice6 )
 
     val mHistory = mutableListOf<Pair<Int, Int>>()
+
     val hi = "Hello World"
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dices = resources.getIntArray(R.array.numberOfDices)
+        val spinner: Spinner = findViewById<Spinner>(R.id.planets_spinner)
+        if (spinner != null){
+            val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item, dices.toList())
+            spinner.adapter = adapter
+        }
     }
 
     fun onClickRoll(v: View){
+        /*
         val d1 = mGenerator.nextInt(6) + 1
         val d2 = mGenerator.nextInt(6) + 1
 
@@ -35,14 +50,22 @@ class MainActivity : AppCompatActivity() {
         if (mHistory.size >= 5)
             mHistory.removeAt(0)
         mHistory.add(Pair(d1, d2))
-        updateHistory()
+        updateHistory()*/
+
+        val spinner: Spinner = findViewById<Spinner>(R.id.planets_spinner)
+        var number = 0
+        for (i in 1..spinner.selectedItem as Int){
+            number = Dice(6).roll()
+            println(number)
+        }
+        imgDice1.setImageResource(diceIds[number])
+
     }
 
     private fun updateHistory(){
         var s = ""
         mHistory.forEach{ p -> val e1 = p.first; val e2 = p.second
                                     s += "$e1 - $e2\n"}
-        tvHistory.text = s
     }
 
 
@@ -53,4 +76,10 @@ class MainActivity : AppCompatActivity() {
         startActivity(i)
     }
 
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
 }
